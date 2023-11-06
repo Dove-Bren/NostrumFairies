27218d6ff6c96f25ed19f7b70397b4b0eb702e72
Skyler Manzanares <skymanzanares@hotmail.com>
2023-11-06 15:48:28 -0800

    Path and ore dict (removal)

--- src/main/java/com/smanzana/nostrumfairies/utils/Paths.java
+++ src/main/java/com/smanzana/nostrumfairies/utils/Paths.java
@@ -2,6 +2,7 @@ package com.smanzana.nostrumfairies.utils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import com.google.common.collect.Lists;
 import com.smanzana.nostrumfairies.entity.navigation.PathPublic;
@@ -13,15 +14,13 @@ import net.minecraft.util.math.BlockPos;
 public final class Paths {
 	
 	public static Path TrimStart(Path path) {
-		PathPoint points[] = new PathPoint[path.getCurrentPathLength() - 1];
+		List<PathPoint> points = new ArrayList<PathPoint>(path.getCurrentPathLength() - 1);
 		// NOTE: start at 1 to chop of 'start' which is usually in a block lol
-		for (int i = 1; i < path.getCurrentPathLength(); i++) {
-			// isn't this dumb? lol. Wish there was .getPathPoints();
-			PathPoint point = path.getPathPointFromIndex(i);
-			points[i-1] = new PathPoint(point.x, point.y, point.z);
+		for (PathPoint point : path.func_215746_d()) {
+			points.add(new PathPoint(point.x, point.y, point.z));
 		}
 		
-		return new Path(points);
+		return new Path(points, path.func_224770_k(), path.func_224771_h());
 	}
 
 	/**
@@ -36,6 +35,8 @@ public final class Paths {
 	public static Path Combine(Collection<Path> paths) {
 		ArrayList<PathPoint> points = new ArrayList<>(paths.size() * 5); // estimate
 		boolean first = true;
+		BlockPos finalDest = null;
+		boolean finalFlagged = false;
 		for (Path path : paths) {
 			// NOTE: start at 1 to chop of 'start' which is usually in a block lol
 			for (int i = first ? 0 : 1; i < path.getCurrentPathLength(); i++) {
@@ -44,9 +45,11 @@ public final class Paths {
 				points.add(new PathPoint(point.x, point.y, point.z));
 			}
 			first = false;
+			finalDest = path.func_224770_k();
+			finalFlagged = path.func_224771_h();
 		}
 		
-		return new Path(points.toArray(new PathPoint[points.size()]));
+		return new Path(points, finalDest, finalFlagged);
 	}
 	
 	/**
